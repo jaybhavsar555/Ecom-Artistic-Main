fc1676fedead6f38a0dbd51299e0c3359c618bbd
Jay Bhavsar <jaybhavsar45007@gmail.com>
2023-04-16 21:56:56 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Laratrust\Traits\LaratrustUserTrait;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements  CanResetPasswordContract
 {
     use LaratrustUserTrait;
     use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable, CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
--- resources/views/layouts/inc/adminavbar.blade.php
+++ resources/views/layouts/inc/adminavbar.blade.php
@@ -28,8 +28,11 @@
                                         </a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
-                                      </form>
-                                  </li>
+                                    </form>
+                                    </li>
+                                    
+             
+            
             
         </div>
       </div>
